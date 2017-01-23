bb801aae82a5f8175ae28b3eae01c241bea6fe53
Sindicollo <sindicollo@gmail.com>
2017-01-23 11:41:31 +0300

    First draft

---
+++ src/pages/Detail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Chance from 'chance';
+
+class Detail extends React.Component {
+    buttonClicked() {
+        this.forceUpdate();
+    }
+
+    render() {
+        return (<div>
+            <p>Hello, {chance.first()}.</p>
+            <p>You are from {chance.country({ full: true })}.</p>
+            <button onClick={this.buttonClicked.bind(this)}>Meet Someone New </button>
+        </div>);
+    }
+}
+
+export default Detail;
