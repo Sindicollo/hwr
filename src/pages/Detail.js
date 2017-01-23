107982bd60f4b6d347ff172a33511912162449ff
Sindicollo <sindicollo@gmail.com>
2017-01-23 14:58:17 +0300

    Added selection from 3 feeds

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,34 +1,120 @@
 import React, { Component } from 'react';
 import Chance from 'chance';
+import ajax from 'superagent';
+
+const SELECTEDVIEW = {
+        COMMITS: 0,
+        FORKS: 1,
+        PULLS: 2,
+}
 
 class Detail extends React.Component {
     constructor(props) {
         super(props);
 
-        const people = [];
-
-        for (let i = 0; i < 10; i++) {
-            people.push({
-                name: chance.first(),
-                country: chance.country({ full: true })
-            });
-        }
-
-        this.state = { people };
+        this.state = { commits: [],
+                       forks: [],
+                       pulls: [],
+                       selectedView: SELECTEDVIEW.COMMITS
+                     };
     }
 
-    buttonClicked() {
+    selectView(view) {
         const newState = {
-            name: chance.first()
+            selectedView: view
         };
+        console.log("Select: " + view);
         this.setState(newState);
+    }
+
+    componentWillMount() {
+        ajax.get('https://api.github.com/repos/facebook/react/commits')
+            .end((error, response) => {
+                if (!error && response) {
+                    console.dir(response.body);
+                    this.setState({ commits: response.body });
+                } else {
+                    console.log('There was an error fetching Commits from GitHub', error);
+                }
+            }
+        );
+
+        ajax.get('https://api.github.com/repos/facebook/react/forks')
+            .end((error, response) => {
+                if (!error && response) {
+                    console.dir(response.body);
+                    this.setState({ forks: response.body });
+                } else {
+                    console.log('There was an error fetching Forks from GitHub', error);
+                }
+            }
+        );
+
+        ajax.get('https://api.github.com/repos/facebook/react/pulls')
+            .end((error, response) => {
+                if (!error && response) {
+                    console.dir(response.body);
+                    this.setState({ pulls: response.body });
+                } else {
+                    console.log('There was an error fetching Pulls from GitHub', error);
+                }
+            }
+        );
+    }
+
+    renderCommits() {
+        return (<div>
+            {
+                this.state.commits.map((commit, index) => {
+                    const author = commit.author ? commit.author.login : 'Anonymous';
+
+                    return (<p key={index}>
+                        <strong>{author}</strong>:
+                        <a href={commit.html_url}>{commit.commit.message}</a>.
+                    </p>);
+                })
+            }
+        </div>);
+    }
+
+    renderForks() {
+        return (<div>
+            {
+                this.state.forks.map((fork, index) => {
+                    return (<p key={index}>
+                        <strong>owner.login</strong>:
+                        <a href={fork.owner.login}>{fork.owner.login}</a>.
+                    </p>);
+                })
+            }
+        </div>);
+    }
+
+    renderPulls() {
+        return (<div>
+            {
+                this.state.pulls.map((pull, index) => {
+                     return (<p key={index}>
+                        <strong>url</strong>:
+                        <a href={pull.url}>{pull.url}</a>.
+                    </p>);
+                })
+            }
+        </div>);
     }
 
     render() {
         return (<div>
-            {this.state.people.map((person, index) => (
-                <p key={index}>Hello, {person.name} from {person.country}!</p>
-        ))}
+            <button onClick={this.selectView.bind(this, SELECTEDVIEW.COMMITS)}>Commits</button>
+            <button onClick={this.selectView.bind(this, SELECTEDVIEW.FORKS)}>Forks</button>
+            <button onClick={this.selectView.bind(this, SELECTEDVIEW.PULLS)}>Pulls</button>
+            {
+                this.state.selectedView === SELECTEDVIEW.COMMITS ? 
+                this.renderCommits() :
+                (this.state.selectedView === SELECTEDVIEW.FORKS ? 
+                 this.renderForks() : 
+                 this.renderPulls())
+            }
         </div>);
     }
 }
