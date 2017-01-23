5761c84bedd7d80918cc15ebbc4b044b5e143ead
Sindicollo <sindicollo@gmail.com>
2017-01-23 17:21:25 +0300

    Added work with react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, useRouterHistory, hashHistory } from 'react-router';
 
 import Detail from './pages/Detail';
+import List from './pages/List';
 
-ReactDOM.render( <
-    Detail message = "This is coming from props!" / > ,
+//For old react router versions
+//import { createHashHistory } from 'history/createHashHistory';
+//const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
+ReactDOM.render(
+    <Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
+        <Route path="/" component={ List } />
+        <Route path="/detail/:repo" component={ Detail } />
+    </Router>,
     document.getElementById('app')
 );
